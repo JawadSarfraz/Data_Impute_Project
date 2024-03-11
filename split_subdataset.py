b6822b71e930c3f63b841f9d1a54103436a38195
JawadSarfraz <jawadsarfraz95@gmail.com>
2024-03-11 22:37:44 +0100

    Split iData n dDfferent Categories

---
+++ split_subdataset.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+
+# Load the Excel file
+file_path = 'dataset_rounded.xlsx'
+xls = pd.ExcelFile(file_path)
+
+# Base directory where the individual datasets will be stored
+base_dir = 'data_impute_project/data/'
+os.makedirs(base_dir, exist_ok=True)
+
+# Loop & Load through each sheet in Excel file
+for sheet_name in xls.sheet_names:
+    # Load the sheet into a dataframe
+    df = pd.read_excel(xls, sheet_name=sheet_name)
+    filename = f"{sheet_name.replace(' ', '_').replace('(', '').replace(')', '')}.xlsx"
+    full_path = os.path.join(base_dir, filename) # path to save
+    
+    df.to_excel(full_path, index=False) # Save dataframe to Excel
+    print(f"Saved {sheet_name} data to {full_path}")
