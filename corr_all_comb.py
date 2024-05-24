4610c170b7e3d11ce942e1426ce0d2c7a5366b27
JawadSarfraz <jawadsarfraz95@gmail.com>
2024-05-24 05:37:12 +0200

    Comments Added to Script

--- corr_all_comb.py
+++ corr_all_comb.py
@@ -3,12 +3,15 @@ import os
 
 def calculate_and_save_correlation_matrices(files, input_base_path, output_base_path):
     """
-    Calculate correlation matrices for list of files and saves each matrix to a new Excel file.
+    Processes list of files, calculates correlation matrices from Excel data, and saves each matrix to new Excel file.
     
     Params:
-    files (list): list of filenames to process.
-    input_base_path (str): base path where input files are located.
-    output_base_path (str): base path where output files will be saved.
+    files: list of filenames to process
+    input_base_path:Base path where the input Excel files are located. This function takes list of filenames (`files`),
+    reads Excel files from `input_base_path`, calculates the correlation matrices for each file, and saves the resulting
+    correlation matrices as new Excel files.
+    output_base_path: Base path where output files will be saved. It is directory where newly created Excel files 
+    containing correlation matrices will be stored
     """
     for file in files:
         try:
@@ -39,6 +42,8 @@ if __name__ == "__main__":
     output_base_path = "data_impute_project/corr_factor"
     
     # List of Files to process
+    # List contains filenames of Excel files to be processed. These filenames represent different datasets 
+    # related to mammals, fish, and birds etc.
     files = [
         'terrestrial_mammals.xlsx',
         'marine_mammals.xlsx',
