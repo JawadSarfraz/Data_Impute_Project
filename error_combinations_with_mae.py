0b9e91803a656716700771551e448459d8f5cfff
JawadSarfraz <jawadsarfraz95@gmail.com>
2024-05-25 16:26:45 +0200

    Commented code and DeletediIrrevalent Files

--- error_combinations_with_mae.py
+++ error_combinations_with_mae.py
@@ -8,6 +8,18 @@ import time
 
 
 def calculate_mae_filtered(original, imputed):
+    """
+    The function calculates mean absolute error (MAE) between two arrays(Columns)
+    after filtering out zero differences.
+    
+    Params:
+    original: array contains orignal column values before removal of values.
+    imputed: array contains values after imputation of missing values.
+
+    return: The function returns Mean Absolute Error (MAE) between
+    original and imputed values, but only for non-zero differences. If there are no non-zero
+    differences, it returns 0 to avoid division by zero.
+    """
     # Calculate differences and filter out zero differences
     differences = original - imputed
     filtered_original = original[differences != 0]
@@ -20,12 +32,46 @@ def calculate_mae_filtered(original, imputed):
         return 0  # Return 0 if there are no differences to avoid division by zero
 
 def ensure_dir(directory):
+    """
+    The function `ensure_dir` creates a directory if it does not already exist.    
+    """
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+
+
+
+
 def get_feature_sets(features, current_feature):
-    """Generate all combinations of features excluding the current feature."""
-    return [''.join(comb) for i in range(1, len(features)) for comb in combinations(features, i) if current_feature in comb]
+    """
+    Generate all combinations of features from the provided list while ensuring that each combination includes
+    the specified 'current_feature'.
+
+    Params:
+    features (list): List of all features available.
+    current_feature (str): The feature that must be included in all combinations.
+
+    Returns:
+    list: A list of feature combinations, each as a concatenated string, where 'current_feature' is included in each combination.
+    """
+    # Initialize an empty list to store feature combinations
+    feature_sets = []
+
+    # Loop over all possible lengths for combinations, from 1 up to the number of features available
+    for i in range(1, len(features) + 1):
+        # Generate all combinations of features of length 'i'
+        for comb in combinations(features, i):
+            # Check if the 'current_feature' is in this combination
+            if current_feature in comb:
+                # If 'current_feature' is in the combination, convert the tuple of features to a concatenated string
+                # and add it to the list of feature sets
+                feature_sets.append(''.join(comb))
+
+    # Return the list of feature sets that include 'current_feature'
+    return feature_sets
+
+
+
 
 # Start the timer
 start_time = time.time()
@@ -43,6 +89,8 @@ algorithms = ['KNN', 'BayesianRidge', 'RandomForest']
 # Store final results
 final_results = []
 
+# Iterating over different combinations of features, algorithms, percentages, and seeds to calculate Mean
+# Absolute Error (MAE) for imputed data compared to original data.
 for combination in combinations_list:
     original_file_path = os.path.join(base_original_path, f"{combination}.xlsx")
     if os.path.exists(original_file_path):
@@ -82,7 +130,7 @@ for combination in combinations_list:
 
 # Create DataFrame and save to CSV
 df_final = pd.DataFrame(final_results, columns=['Combination', 'Feature', 'Percentage', 'Algorithm', 'Min MAE', 'FeatureSet Removal Scenario'])
-output_file_path = os.path.join(output_dir, 'min_error_ChangeVal.csv')
+output_file_path = os.path.join(output_dir, 'test.csv')
 df_final.to_csv(output_file_path, index=False)
 
 # End the timer and print the running time
